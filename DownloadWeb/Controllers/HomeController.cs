89b031261123480f414d4f4f8451a3b742ea2c40
EatMedicine <870830676@qq.com>
2018-10-28 01:15:59 +0800

    完成初步

--- DownloadWeb/Controllers/HomeController.cs
+++ DownloadWeb/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,23 +9,50 @@ namespace DownloadWeb.Controllers
 {
     public class HomeController : Controller
     {
+        private static string path = @"C:\WebDownload\";
         public ActionResult Index()
         {
             return View();
         }
-
-        public ActionResult About()
+        public ActionResult Download()
         {
-            ViewBag.Message = "Your application description page.";
-
+            string path = @"C:\WebDownload\";
+            try
+            {
+                DirectoryInfo info = new DirectoryInfo(path);
+                List<string> filename = new List<string>();
+                foreach (FileInfo fi in info.GetFiles())
+                {
+                    filename.Add(fi.FullName);
+                }
+                ViewBag.FileName = filename.ToArray();
+            }
+            catch
+            {
+                
+            }
             return View();
         }
 
-        public ActionResult Contact()
+        public FileStreamResult fileDownload(int id)
         {
-            ViewBag.Message = "Your contact page.";
+            DirectoryInfo info = new DirectoryInfo(path);
+            List<string> filename = new List<string>();
+            foreach (FileInfo fi in info.GetFiles())
+            {
+                filename.Add(fi.FullName);
+            }
+            for (int count = 0; count < filename.Count; count++)
+            {
+                if (count == id) 
+                {
+                    string fullname = info.GetFiles()[count].FullName;
+                    FileStream fs = new FileStream(fullname, FileMode.Open);
+                    return File(fs, "application/octet-stream", filename[count]);
+                }
+            }
+            return null;
 
-            return View();
         }
     }
 }
